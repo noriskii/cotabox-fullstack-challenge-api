f4e01ba61dc469ef08d9bcfdd0e39771b958b295
Lucas Augusto <luckaugusto@gmail.com>
2020-07-21 14:13:00 -0300

    Add API backend

---
+++ src/controllers/ContributorController.js
@@ -0,0 +1,80 @@
+
+const axios = require('axios');
+const Contributor = require('../models/contributor');
+
+module.exports = {
+
+  async index(req, res) {
+    await Contributor.find((err, contributors) => {
+      if(err) { 
+        return handleError(res, err); 
+      }
+
+      return res.status(200).json(contributors);
+    });
+  },
+
+  async reset(req, res) {
+    await Contributor.deleteMany({}, (err, contributor) => {
+      if(err) { 
+        return handleError(res, err); 
+      }
+
+      return res.status(200).json(contributor);
+    });
+  },
+
+  async store(req, res) {
+    const { firstName, lastName, contribution } = req.body;
+
+    const userExists = await Contributor.findOne({ firstName, lastName });
+
+    if(userExists) {
+      return res.json(userExists);
+    }
+    
+    await Contributor.create({
+      firstName,
+      lastName,
+      contribution
+    }, (err, contributor) => {
+      if(err) { 
+        return handleError(res, err); 
+      }
+
+      return res.status(200).json(contributor);
+    });    
+  },
+
+
+  async remove(req, res) {
+    const { firstName, lastName } = req.body;
+
+    await Contributor.deleteOne({
+      firstName,
+      lastName
+    }, (err, contributor) => {
+      if(err) { 
+        return handleError(res, err); 
+      }
+
+      return res.status(200).json(contributor);
+    });    
+  },
+
+  async percentage(req, res) {
+    const percentage = await Contributor.aggregate([{
+      $match : { },
+    },{
+      $group : {
+        _id: null,
+        total : {
+            $sum : "$contribution"
+        }
+      }
+    }]);
+
+    return res.status(200).json(percentage[0]);
+  }
+
+}
